8348bc41354a595281de27ab6e7151a1adb87321
jungik <jungik75@naver.com>
2024-10-18 12:51:12 +0900

    ver3.0 - (ver_B)

--- cal.c
+++ cal.c
@@ -4,9 +4,10 @@ int main()
 {
 	int num1, num2;
 	
-	num1 = 3;
-	num2 = 5;
+	printf("Enter the num1, num2 : ");
+	scanf("%d %d", &num1, &num2);
 
+	printf("The result of calculation\n");
 	printf("%d + %d = %d\n", num1, num2, num1+num2);
         printf("%d + %d = %d\n", num1, num2, num1+num2);
         printf("%d + %d = %d\n", num1, num2, num1+num2);
